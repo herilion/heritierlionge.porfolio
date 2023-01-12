adfaafcefffaa85024ec8974a617de66dcf83202
herilion <heritierlionge@yahoo.com>
2023-01-12 16:57:54 +0200

    hide style-switcher on scrole

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -3,3 +3,9 @@ const styleSwitcherToggle = document.querySelector('.style-switcher-toggler');
 styleSwitcherToggle.addEventListener('click', () => {
     document.querySelector('.style-switcher').classList.toggle('open');
 })
+// hide style-switcher on scroll
+window.addEventListener('scroll', () => {
+    if (document.querySelector('.style-switcher').classList.contains('open')) {
+        document.querySelector('.style-switcher').classList.remove('open')
+    }
+})
