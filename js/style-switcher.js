3dd544721a11f3e5751bd7d4edecb079824ff4bb
herilion <heritierlionge@yahoo.com>
2023-01-18 16:51:17 +0200

    add theme ligth and dark

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -25,7 +25,8 @@ function setActiveStyle(color) {
 const dayNight = document.querySelector('.day-night');
 dayNight.addEventListener('click', () => {
     dayNight.querySelector('i').classList.toggle('fa-sun');
-    dayNight.querySelector('i').classList.toggle('fa-moon')
+    dayNight.querySelector('i').classList.toggle('fa-moon');
+    document.body.classList.toggle('dark');
 })
 window.addEventListener('load', () => {
     if (document.body.classList.contains('dark')) {
