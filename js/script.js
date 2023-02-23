d3510edc1dc036ebd759d19af38f93c59aaabd00
herilion <heritierlionge@yahoo.com>
2023-02-23 15:49:42 +0200

    update typeSpeed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // typing animation
 var typed = new Typed('.typing', {
-    strings: ["", "SoftWare Engineer", "FullStack developper", "From Goma, DRC"],
+    strings: ["", "SoftWare Engineer", "FullStack web developper", "From Goma, DRC"],
     typeSpeed: 100,
     BackSpeed: 60,
     loop: true
