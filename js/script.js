e9e9c31899be6bb393f9c357e822a8936d130c6b
herilion <heritierlionge@yahoo.com>
2023-02-23 15:23:58 +0200

    add cursor to switcher

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 // typing animation
 var typed = new Typed('.typing', {
-    strings: ["SoftWare Engineer", "FullStack developper", "From Goma, DRC"]
+    strings: ["", "SoftWare Engineer", "FullStack developper", "From Goma, DRC"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
 })
