fd4307039d18d059c2449f503b21d0c046c31e25
herilion <heritierlionge@yahoo.com>
2023-03-16 16:52:45 +0200

    update aside

--- js/script.js
+++ js/script.js
@@ -4,4 +4,16 @@ var typed = new Typed('.typing', {
     typeSpeed: 100,
     BackSpeed: 60,
     loop: true
-})
+});
+// aside
+
+const nav = document.querySelector('.nav'),
+    navList = nav.querySelectorAll('li'),
+    totalNavList = navList.length;
+for (let i = 0; i < totalNavList; i++) {
+
+    const a = navList[i].querySelector('a');
+    a.addEventListener('click', () => {
+        console.log(this);
+    })
+}
