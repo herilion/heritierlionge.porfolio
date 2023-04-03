0aa8fbf79ac8702394ff4340c07ae84a5a7b48de
herilion <heritierlionge@yahoo.com>
2023-04-03 11:11:33 +0200

    update anchor

--- js/script.js
+++ js/script.js
@@ -7,12 +7,17 @@ var typed = new Typed('.typing', {
 });
 // aside
 
-const nav = document.querySelector('.nav'),
-    navList = nav.querySelectorAll('li'),
-    totalNavList = navList.length;
-for (let i = 0; i < totalNavList; i++) {
-    const a = navList[i].querySelector('a');
-    a.addEventListener('click', () => {
-        console.log(this);
-    })
-}
+// const nav = document.querySelector('.nav'),
+//     navList = nav.querySelectorAll('li'),
+//     totalNavList = navList.length;
+// for (let i = 0; i < totalNavList; i++) {
+//     const a = navList[i].querySelector('a');
+//     a.addEventListener('click', () => {
+//         this.classList.add('active');
+//     })
+// }
+const hcol=document.querySelector('.nav li');
+hcol.addEventListerner('click',()=>{
+    hcol.style.color='red';
+    alert('Heritier LIONGE')
+});
