8ece20166edb8e2773132ae386bb01097f317b58
herilion <heritierlionge@yahoo.com>
2023-06-09 21:39:09 +0200

    update

--- js/script.js
+++ js/script.js
@@ -4,31 +4,32 @@ var typed = new Typed('.typing', {
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
-});
-
-// management aside
-const allSection=document.querySelectorAll(".section");
-const totalSection=allSection.length;
-const nav = document.querySelector(".nav"),
-    navList = nav.querySelectorAll('li'),
-    totalNavList = navList.length;
-    
-for (let i = 0; i < totalNavList; i++) {
+  });
+  
+  // management aside
+  const allSection = document.querySelectorAll(".section");
+  const totalSection = allSection.length;
+  const nav = document.querySelector(".nav");
+  const navList = nav.querySelectorAll('li');
+  const totalNavList = navList.length;
+  
+  for (let i = 0; i < totalNavList; i++) {
     const a = navList[i].querySelector('a');
     a.addEventListener('click', (event) => {
-        event.preventDefault();
-        for(let j=0;j<totalNavList;j++){
-            navList[j].querySelector("a").classList.remove("active");
-        }
-        a.classList.add("active");
-        showSection(a);
+      event.preventDefault();
+      for (let j = 0; j < totalNavList;j++) {
+        navList[j].querySelector("a").classList.remove("active");
+      }
+      a.classList.add("active");
+      showSection(a);
     });
-}
-const showSection = (a) => {
-    console.log(a)
-    for(let i=0; i<totalSection; i++){
-        allSection[i].classList.remove("active");
+  }
+  
+  const showSection = (a) => {
+    for (let i = 0; i < totalSection; i++) {
+      allSection[i].classList.remove("active");
     }
-    const target = element.getAttribute("href").split("#")[1];
+    const target = a.getAttribute("href").split("#")[1];
     document.querySelector("#" + target).classList.add("active");
-}
+  }
+  
