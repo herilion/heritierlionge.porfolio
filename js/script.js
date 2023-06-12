07ad52a4277406ca16ab9b3e0f1c350665e8dbe7
herilion <heritierlionge@yahoo.com>
2023-06-12 15:04:22 +0200

    manage back section

--- js/script.js
+++ js/script.js
@@ -17,9 +17,12 @@ var typed = new Typed('.typing', {
     const a = navList[i].querySelector('a');
     a.addEventListener('click', (event) => {
       event.preventDefault();
+      for (let i = 0; i < totalSection; i++) {
+        allSection[i].classList.remove("back-section");
+      }
       for (let j = 0; j < totalNavList;j++) {
         if(navList[j].querySelector("a").classList.contains("active")){
-          console.log(navList[i].querySelector("a"))
+          allSection[j].classList.add("back-section");
         }
         navList[j].querySelector("a").classList.remove("active");
       }
